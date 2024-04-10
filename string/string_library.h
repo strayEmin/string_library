7ba92e45579005c1fcdf6964ecb5063105322847
Pavel Chernyshev <bungostraygit@gmail.com>
2024-04-10 22:52:26 +0300

    search, compare and copy functions have been initialized

--- string/string_library.h
+++ string/string_library.h
@@ -1,4 +1,43 @@
 #ifndef STR_STRING_LIBRARY_H
 #define STR_STRING_LIBRARY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <ctype.h>
+
+
+size_t strlen_(const char *begin);
+
+
+char* find(char *begin, char *end, int ch);
+
+
+char* findNonSpace(char *begin);
+
+
+char* findSpace(char *begin);
+
+
+char* findNonSpaceReverse(char *rbegin, const char *rend);
+
+
+char* findSpaceReverse(char *rbegin, const char *rend);
+
+
+int strcmp_(const char *lhs, const char *rhs);
+
+
+char* copy(const char *beginSource, const char *endSource,
+           char *beginDestination);
+
+
+char* copyIf(char *beginSource, const char *endSource,
+             char *beginDestination, int (*f)(int));
+
+
+char* copyIfReverse(char *rbeginSource, const char *rendSource,
+                    char *beginDestination, int (*f)(int));
+
+
 #endif //STR_STRING_LIBRARY_H
