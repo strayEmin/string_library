1e9eb329ebc122372f423de2b31c0b656297a9f5
Pavel Chernyshev <bungostraygit@gmail.com>
2024-04-11 19:20:50 +0300

    added & tested search functions

--- string/string_library.c
+++ string/string_library.c
@@ -1,5 +1,50 @@
-//
-// Created by пашк on 09.04.2024.
-//
-
 #include "string_library.h"
+
+
+size_t strlen_(char *begin) {
+    char *end = begin;
+    while (*end != '\0')
+        end++;
+
+    return end - begin;
+}
+
+
+char* find(char *begin, char *end, int ch) {
+    while (begin != end && *begin != ch)
+        begin++;
+
+    return begin;
+}
+
+
+char* findNonSpace(char *begin) {
+    while (*begin != '\0' && isspace(*begin))
+        begin++;
+
+    return begin;
+}
+
+
+char* findSpace(char *begin) {
+    while (*begin != '\0' && !isspace(*begin))
+        begin++;
+
+    return begin;
+}
+
+
+char* findNonSpaceReverse(char *rbegin, const char *rend) {
+    while (rbegin != rend && isspace(*rbegin))
+        rbegin--;
+
+    return rbegin;
+}
+
+
+char* findSpaceReverse(char *rbegin, const char *rend) {
+    while (rbegin != rend && !isspace(*rbegin))
+        rbegin--;
+
+    return rbegin;
+}
