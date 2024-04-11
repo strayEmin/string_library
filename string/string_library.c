2dac53a33d37adfbf3a5da9402dea5f3be02710f
Pavel Chernyshev <bungostraygit@gmail.com>
2024-04-11 20:55:37 +0300

    added & tested copy functions

--- string/string_library.c
+++ string/string_library.c
@@ -10,7 +10,7 @@ size_t strlen_(char *begin) {
 }
 
 
-char* find(char *begin, char *end, int ch) {
+char *find(char *begin, char *end, int ch) {
     while (begin != end && *begin != ch)
         begin++;
 
@@ -18,7 +18,7 @@ char* find(char *begin, char *end, int ch) {
 }
 
 
-char* findNonSpace(char *begin) {
+char *findNonSpace(char *begin) {
     while (*begin != '\0' && isspace(*begin))
         begin++;
 
@@ -26,7 +26,7 @@ char* findNonSpace(char *begin) {
 }
 
 
-char* findSpace(char *begin) {
+char *findSpace(char *begin) {
     while (*begin != '\0' && !isspace(*begin))
         begin++;
 
@@ -34,7 +34,7 @@ char* findSpace(char *begin) {
 }
 
 
-char* findNonSpaceReverse(char *rbegin, const char *rend) {
+char *findNonSpaceReverse(char *rbegin, const char *rend) {
     while (rbegin != rend && isspace(*rbegin))
         rbegin--;
 
@@ -42,7 +42,7 @@ char* findNonSpaceReverse(char *rbegin, const char *rend) {
 }
 
 
-char* findSpaceReverse(char *rbegin, const char *rend) {
+char *findSpaceReverse(char *rbegin, const char *rend) {
     while (rbegin != rend && !isspace(*rbegin))
         rbegin--;
 
@@ -51,10 +51,50 @@ char* findSpaceReverse(char *rbegin, const char *rend) {
 
 
 int strcmp_(const char *lhs, const char *rhs) {
-    while(*lhs == *rhs && *lhs != '\0') {
+    while (*lhs == *rhs && *lhs != '\0') {
         lhs++;
         rhs++;
     }
 
     return *lhs - *rhs;
+}
+
+
+char *copy(const char *begin_source, const char *end_source, char *begin_destination) {
+    size_t size = end_source - begin_source;
+    memcpy(begin_destination, begin_source, size);
+
+    return begin_destination + size;
+}
+
+
+char *copyIf(char *begin_source, const char *end_source, char *begin_destination, int (*f)(int)) {
+    while (begin_source != end_source) {
+        if (f(*begin_source)) {
+            *begin_destination = *begin_source;
+            begin_destination++;
+        }
+
+        begin_source++;
+    }
+
+    *begin_destination = '\0';
+
+    return begin_destination;
+}
+
+
+char *copyIfReverse(char *rbegin_source, const char *rend_source, char *begin_destination, int (*f)(int)) {
+    while (rbegin_source != rend_source) {
+        if (f(*rbegin_source)) {
+            *begin_destination = *rbegin_source;
+            begin_destination++;
+        }
+
+        rbegin_source--;
+    }
+
+    *begin_destination = '\0';
+
+    return begin_destination;
 }
