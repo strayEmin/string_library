fc579efcbd28fd51a6375785a237f8a709ff391b
Pavel Chernyshev <bungostraygit@gmail.com>
2024-04-11 19:37:22 +0300

    added & tested func strcmp_

--- string/string_library.c
+++ string/string_library.c
@@ -48,3 +48,13 @@ char* findSpaceReverse(char *rbegin, const char *rend) {
 
     return rbegin;
 }
+
+
+int strcmp_(const char *lhs, const char *rhs) {
+    while(*lhs == *rhs && *lhs != '\0') {
+        lhs++;
+        rhs++;
+    }
+
+    return *lhs - *rhs;
+}
